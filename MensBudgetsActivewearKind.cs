004a99e85f683b64e398c3a95d893075d6066b0e
hecThorB <hectorobeltran@gmail.com>
2017-02-14 10:28:06 -0800

    Update MensBudgetsActivewearKind.cs

--- MensBudgetsActivewearKind.cs
+++ MensBudgetsActivewearKind.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Sabio.Web.Enums
+namespace FoundIt.Web.Enums
 {
     public enum MensBudgetsActivewearKind
     {
