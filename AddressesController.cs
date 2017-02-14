bed11e794bfce5536915521ccb0c5421b2148809
hecThorB <hectorobeltran@gmail.com>
2017-02-14 10:11:22 -0800

    Update AddressesController.cs

--- AddressesController.cs
+++ AddressesController.cs
@@ -1,11 +1,11 @@
-﻿using Sabio.Web.Models.ViewModels;
+﻿using Foundit.Web.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Sabio.Web.Controllers
+namespace Foundit.Web.Controllers
 {
     [RoutePrefix("addresses")]
     public class AddressesController : BaseController
