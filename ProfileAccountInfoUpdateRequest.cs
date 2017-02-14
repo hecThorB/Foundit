88fae129b1d3fa872688f67fd77b43172c16fb66
hecThorB <hectorobeltran@gmail.com>
2017-02-14 10:30:16 -0800

    Update ProfileAccountInfoUpdateRequest.cs

--- ProfileAccountInfoUpdateRequest.cs
+++ ProfileAccountInfoUpdateRequest.cs
@@ -6,7 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace Sabio.Web.Models.Requests
+namespace FoundIt.Web.Models.Requests
 {
     public class ProfileAccountInfoUpdateRequest : ProfileAccountInfoCreateRequest
     {
