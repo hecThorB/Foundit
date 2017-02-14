76861b0e0724d2b98811c93d1f77f8dab1acc94b
hecThorB <hectorobeltran@gmail.com>
2017-02-14 10:50:52 -0800

    Update ProfileMensBudgetsUpdateRequest.cs

--- ProfileMensBudgetsUpdateRequest.cs
+++ ProfileMensBudgetsUpdateRequest.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Sabio.Web.Models.Requests
+namespace FoundIt.Web.Models.Requests
 {
     public class ProfileMensBudgetsUpdateRequest : ProfileMensBudgetsCreateRequest
     {
