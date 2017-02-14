6ff7b2a1ff24d6f2247222b206a555ea24c35a79
hecThorB <hectorobeltran@gmail.com>
2017-02-14 10:49:42 -0800

    Update ProfileMensBudgetsController.cs

--- ProfileMensBudgetsController.cs
+++ ProfileMensBudgetsController.cs
@@ -1,11 +1,11 @@
-﻿using Sabio.Web.Models.ViewModels;
+﻿using FoundIt.Web.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Sabio.Web.Controllers
+namespace FoundIt.Web.Controllers
 {
     [RoutePrefix("profiles/budgets")]
     public class ProfileMensBudgetsController : BaseController
